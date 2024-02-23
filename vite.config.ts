f883e32be1b209786fdbbea398223a2759fef7ff
Paul Teasdale <teasdalep@gmail.com>
2024-02-23 14:24:09 -0500

    wip - config...

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 /** @type {import('vite').UserConfig} */
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig(({ command, mode }) => {
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '')
 
-    if (command === 'serve') {
-        return {};
-    } else {
-        // return {};        
-        // command === 'build'
-        return {
-            base: "/StarForge/"
-        }
+    // if (command === 'serve') {
+    //     return {};
+    // } else {
+    //     // return {};        
+    //     // command === 'build'
+    return {
+        base: "/StarForge/"
     }
-
 });
