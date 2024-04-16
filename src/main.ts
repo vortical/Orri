d8bf3e92ba634959956a94c1083df8b9a34b67f4
Paul Teasdale <teasdalep@gmail.com>
2024-04-16 15:29:42 -0400

    wrap top level await in async function

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,16 @@ import config from './configuration.ts';
 console.log(`Config: \n${JSON.stringify(config)}`);
 const mainElement = document.querySelector<HTMLDivElement>('#scene-container')!;
 
-const dataService = new DataService(config.spacefieldBaseURL, config.baseUrl);
-const bodySystemUpdater = new NBodySystemUpdater();
-const options = LocationBar.getState();
-const date = options.date ? new Date(options.date) : new Date()
-const bodies: Body[] = await dataService.loadSolarSystem(date);
-const bodySystem = new BodySystem(mainElement, bodies, dataService, bodySystemUpdater, options);
-bodySystem.setCameraUp(bodySystem.getBody("earth").get_orbital_plane_normal());
-const ui = new SimpleUI(bodySystem, dataService);
-bodySystem.start();
+(async () => {
+    const dataService = new DataService(config.spacefieldBaseURL, config.baseUrl);
+    const bodySystemUpdater = new NBodySystemUpdater();
+    const options = LocationBar.getState();
+    const date = options.date ? new Date(options.date) : new Date()
+    const bodies: Body[] = await dataService.loadSolarSystem(date);
+    const bodySystem = new BodySystem(mainElement, bodies, dataService, bodySystemUpdater, options);
+    bodySystem.setCameraUp(bodySystem.getBody("earth").get_orbital_plane_normal());
+    const ui = new SimpleUI(bodySystem, dataService);
+    bodySystem.start();
+    
+})();
+
